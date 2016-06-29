a433d006a82953868cb0d87e8ab9ef24a976c259
Kevin <kevin.dou@eeda123.com>
2016-06-29 17:36:17 +0800

    model 入库通知单

---
+++ src/models/eeda/oms/GateInOrderItem.java
@@ -0,0 +1,1 @@
+package models.eeda.oms;import com.jfinal.plugin.activerecord.Model;@SuppressWarnings("serial")public class GateInOrderItem extends Model<GateInOrderItem> {	public static final GateInOrderItem dao = new GateInOrderItem();}
