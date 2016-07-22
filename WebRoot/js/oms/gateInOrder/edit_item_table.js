1aa8584b3170c8444a73e4a6f8c27ae1fa63d6e5
Kevin <kevin.dou@eeda123.com>
2016-07-22 10:50:57 +0800

    入库单明细调整

--- WebRoot/js/oms/gateInOrder/edit_item_table.js
+++ WebRoot/js/oms/gateInOrder/edit_item_table.js
@@ -33,19 +33,19 @@ $(document).ready(function() {
         		UPC: $(row.children[2]).find('input').val(),
         		cargo_name: $(row.children[3]).find('input').val(),
         		paln_amount: $(row.children[4]).find('input').val(),
-        		shelf_life: $(row.children[5]).find('input').val(),
-                carton_no: $(row.children[6]).find('input').val(), 
-                upstream_sku: $(row.children[7]).find('input').val(), 
-                custom_code: $(row.children[8]).find('input').val(),
-                item_code: $(row.children[9]).find('input').val(),
-                name_specifications: $(row.children[10]).find('input').val(),
-                color: $(row.children[11]).find('input').val(),
-                size: $(row.children[12]).find('input').val(), 
-                currency: $(row.children[13]).find('input').val(), 
-                unit_value: $(row.children[14]).find('input').val(),
-                packing_unit: $(row.children[15]).find('select').val(), 
-                packing_amount: $(row.children[16]).find('input').val(), 
-                received_amount: $(row.children[17]).find('input').val(), 
+        		received_amount: $(row.children[5]).find('input').val(), 
+        		shelf_life: $(row.children[6]).find('input').val(),
+                carton_no: $(row.children[7]).find('input').val(), 
+                upstream_sku: $(row.children[8]).find('input').val(), 
+                custom_code: $(row.children[9]).find('input').val(),
+                item_code: $(row.children[10]).find('input').val(),
+                name_specifications: $(row.children[11]).find('input').val(),
+                color: $(row.children[12]).find('input').val(),
+                size: $(row.children[13]).find('input').val(), 
+                currency: $(row.children[14]).find('input').val(), 
+                unit_value: $(row.children[15]).find('input').val(),
+                packing_unit: $(row.children[16]).find('select').val(), 
+                packing_amount: $(row.children[17]).find('input').val(), 
                 damage_amount: $(row.children[18]).find('input').val(), 
                 remark: $(row.children[19]).find('input').val(), 
                 action: id.length>0?'UPDATE':'CREATE'
@@ -66,36 +66,7 @@ $(document).ready(function() {
         return cargo_items_array;
     };
     
-    gateInOrder.reDrawCargoTable=function(order){
-        deletedTableIds=[];
-        cargoTable.clear();
-        for (var i = 0; i < order.ITEM_LIST.length; i++) {
-            var item = order.ITEM_LIST[i];
-            var item={
-                "ID": item.ID,
-                "UPC": item.UPC,
-                "cargo_name": item.CARGO_NAME,
-                "paln_amount": item.PALN_AMOUNT,
-                "shelf_life": item.SHELF_LIFE,
-                "CARTON_NO": item.CARTON_NO,
-                "UPSTREAM_SKU": item.UPSTREAM_SKU,
-                "CUSTOM_CODE": item.CUSTOM_CODE,
-                "ITEM_CODE": item.ITEM_CODE,
-                "NAME_SPECIFICATIONS": item.NAME_SPECIFICATIONS,
-                "COLOR": item.COLOR,
-                "SIZE": item.SIZE,
-                "CURRENCY": item.CURRENCY,
-                "UNIT_VALUE": item.UNIT_VALUE,
-                "PACKING_UNIT": item.PACKING_UNIT,
-                "PACKING_AMOUNT": item.PACKING_AMOUNT,
-                "RECEIVED_AMOUNT": item.RECEIVED_AMOUNT,
-                "DAMAGE_AMOUNT": item.DAMAGE_AMOUNT,
-                "REMARK": item.REMARK
-            };
-    
-            cargoTable.row.add(item).draw(false);
-        }       
-    };
+
 
     //------------事件处理
     var cargoTable = $('#cargo_table').DataTable({
@@ -126,14 +97,14 @@ $(document).ready(function() {
                     return meta.row+1;
                 }
             },
-            { "data": "UPC", 
+            { "data": "UPC", "width": "120px", 
             	"render": function ( data, type, full, meta ) {
             		if(!data)
             			data='';
             		return '<input type="text" value="'+data+'" class="form-control" required/>';
             	}
             },
-            { "data": "CARGO_NAME", 
+            { "data": "CARGO_NAME",  "width": "200px",
             	"render": function ( data, type, full, meta ) {
             		if(!data)
             			data='';
@@ -147,7 +118,14 @@ $(document).ready(function() {
             		return '<input type="text" value="'+data+'" class="form-control" required/>';
             	}
             },
-            { "data": "SHELF_LIFE", 
+            { "data": "RECEIVED_AMOUNT",
+                "render": function ( data, type, full, meta ) {
+                    if(!data)
+                        data='';
+                   return '<input type="text" value="'+data+'" class="form-control" required/>';
+                }
+            },
+            { "data": "SHELF_LIFE", "width": "120px",
             	"render": function ( data, type, full, meta ) {
             		if(!data)
             			data='';
@@ -254,13 +232,7 @@ $(document).ready(function() {
                    return '<input type="text" value="'+data+'" class="form-control" required/>';
                 }
             },
-            { "data": "RECEIVED_AMOUNT",
-                "render": function ( data, type, full, meta ) {
-                    if(!data)
-                        data='';
-                   return '<input type="text" value="'+data+'" class="form-control" required/>';
-                }
-            },
+           
             { "data": "DAMAGE_AMOUNT",
                 "render": function ( data, type, full, meta ) {
                     if(!data)
