c5955155f1e2a1cac5726d7eac236f14a4dfc799
doukevin <kevin.dou@eeda123.com>
2016-08-13 00:08:43 +0800

    运单明细表下拉列表

--- WebRoot/js/oms/logisticsOrder/edit_item_table.js
+++ WebRoot/js/oms/logisticsOrder/edit_item_table.js
@@ -99,7 +99,32 @@ $(document).ready(function() {
                 "render": function ( data, type, full, meta ) {
                     if(!data)
                         data='';
-                   return '<input type="text" disabled value="'+data+'" class="form-control"/>';
+                    var str= '<select class="form-control search-control" name="unit">'
+              	   	   +'<option></option>'
+              	   	   +'<option value="6" '+ (data=='6'?'selected':'') +'>套</option>'
+  	                   +'<option value="7" '+ (data=='7'?'selected':'') +'>个</option>'
+  	                   +'<option value="8" '+ (data=='8'?'selected':'') +'>只</option>'
+  	                   +'<option value="11" '+ (data=='11'?'selected':'') +'>件</option>'
+  	                   +'<option value="12" '+ (data=='12'?'selected':'') +'>支</option>'
+  	                   +'<option value="14" '+ (data=='14'?'selected':'') +'>根</option>'
+  	                   +'<option value="15" '+ (data=='15'?'selected':'') +'>条</option>'
+  	                   +'<option value="17" '+ (data=='17'?'selected':'') +'>块</option>'
+  	                   +'<option value="18" '+ (data=='18'?'selected':'') +'>卷</option>'
+  	                   +'<option value="20" '+ (data=='20'?'selected':'') +'>片</option>'
+  	                   +'<option value="35" '+ (data=='35'?'selected':'') +'>千克</option>'
+  	                   +'<option value="36" '+ (data=='36'?'selected':'') +'>克</option>'
+  	                   +'<option value="70" '+ (data=='70'?'selected':'') +'>吨</option>'
+  	                   +'<option value="75" '+ (data=='75'?'selected':'') +'>斤</option>'
+  	                   +'<option value="96" '+ (data=='96'?'selected':'') +'>毫升</option>'
+  	                   +'<option value="122" '+ (data=='122'?'selected':'') +'>罐</option>'
+  	                   +'<option value="125" '+ (data=='125'?'selected':'') +'>包</option>'
+  	                   +'<option value="134" '+ (data=='134'?'selected':'') +'>枚</option>'
+  	                   +'<option value="136" '+ (data=='136'?'selected':'') +'>袋</option>'
+  	                   +'<option value="139" '+ (data=='139'?'selected':'') +'>粒</option>'
+  	                   +'<option value="140" '+ (data=='140'?'selected':'') +'>盒</option>'
+  	                   +'<option value="142" '+ (data=='142'?'selected':'') +'>瓶</option>'
+ 	                   +'</select>';
+                      return str;
                 }
             },
             { "data": "PRICE",
