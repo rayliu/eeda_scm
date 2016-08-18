aa744555ae5288436307f40d5529237bc9e7a8e5
Kevin <kevin.dou@eeda123.com>
2016-08-18 16:12:52 +0800

    job 自动更新逻辑更改

--- src/controllers/yh/job/CustomJob.java
+++ src/controllers/yh/job/CustomJob.java
@@ -10,6 +10,7 @@ import org.apache.commons.lang.StringUtils;
 
 import models.eeda.OrderActionLog;
 import models.eeda.oms.InspectionOrderItem;
+import models.eeda.oms.LogisticsOrder;
 import models.eeda.oms.SalesOrder;
 
 import com.google.gson.Gson;
@@ -31,7 +32,7 @@ public class CustomJob implements Runnable {
 	public void run() {
 	    logger.debug("更新单据状态开始.....");
 	    
-	    List<Record> record = Db.find("select * from sales_order where ifnull(status,'') != '接收成功'");
+	    List<Record> record = Db.find("select * from sales_order where status != '接收成功' and status != '未上报'");
 	    for(Record re : record){
 	    	String order_no = re.getStr("order_no");
 	    	String order_id = re.getLong("id").toString();
@@ -45,6 +46,8 @@ public class CustomJob implements Runnable {
 	        String order_cus_status = itemList.get(0).get("order_cus_status");
 	        String order_ciq_status = itemList.get(0).get("order_ciq_status");
 	        String pay_status = itemList.get(0).get("pay_status");
+	        String logistics_ciq_status = itemList.get(0).get("logistics_ciq_status");
+	        String logistics_cus_status = itemList.get(0).get("logistics_cus_status");
 	        
 	        
 	        System.out.println(message);
@@ -52,20 +55,41 @@ public class CustomJob implements Runnable {
 	        //保存查询log
 	        operationLog("customStatus",jsonMsg,order_id,"get",null);
 	        
-	        //更新销售订单状态
-	        SalesOrder so = SalesOrder.dao.findById(order_id);
+	        
 	        if(!"订单不存在！".equals(message)){
-	        	so.set("order_cus_status", statusShow(order_cus_status));
-	        	so.set("order_ciq_status", statusShow(order_ciq_status));
-	        	so.set("pay_status",statusShow(pay_status));
-	        	so.set("status", message).update();
+	        	//更新销售订单状态 
+	        	SalesOrder so = SalesOrder.dao.findById(order_id);
+	        	so.set("order_cus_status", statusShow(order_cus_status,"order_cus_status"));
+	        	so.set("order_ciq_status", statusShow(order_ciq_status,"order_ciq_status"));
+	        	so.set("pay_status",statusShow(pay_status,"pay_status"));
+	        	
+	        	if("30".equals(order_cus_status) && "30".equals(order_ciq_status)){
+	        		so.set("status", "已通关");
+	        	}else{
+	        		so.set("status", "报关处理中");
+	        	}
+	        	so.update();
+	        	
+	        	//更新运单状态
+		        LogisticsOrder lo = LogisticsOrder.dao.findFirst("select * from logistics_order where sales_order_id = ?",order_id);
+		        lo.set("logistics_ciq_status", statusShow(logistics_ciq_status,"logistics_ciq_status"));
+	        	lo.set("logistics_cus_status", statusShow(logistics_cus_status,"logistics_cus_status"));
+	        	
+	        	if("30".equals(logistics_ciq_status) && "30".equals(logistics_cus_status)){
+	        		lo.set("status","已通关");
+	        	}else if("".equals(logistics_ciq_status) && "".equals(logistics_cus_status)){
+	        		lo.set("status","未上报");
+	        	} else{
+	        		lo.set("status","报关处理中");
+	        	}
+	        	lo.update();
 	        } 
 	    }
 	    
 	    logger.debug("更新单据状态结束");
 	}
 	
-	public static String statusShow(String status){ 
+	public static String statusShow(String status,String id){ 
     	String massage = "";
     	if("10".equals(status)){
     		massage = "未发送";
@@ -73,8 +97,14 @@ public class CustomJob implements Runnable {
     		massage ="已发送";
     	}else if("30".equals(status)){
     		massage = "接收成功";
+    	}else if("31".equals(status)){
+    		massage = "审批通过";
     	}else if("32".equals(status)){
-    		massage = "接收不成功";
+    		if("bill_cus_status".equals(id) || "bill_ciq_status".equals(id)){
+    			massage = "审批不通过";
+    		}else{
+    			massage = "接收不成功";
+    		}
     	}
     	return massage;
     }
