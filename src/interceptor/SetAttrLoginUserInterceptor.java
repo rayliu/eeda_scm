59116e893b200e51a05b28416847c829d0188cd1
Ray Liu <liuyubie@gmail.com>
2016-08-20 18:07:11 +0800

    SetAttrLoginUserInterceptor:   将登陆用户信息保存到session中, 不需要每次都从数据库中读取

--- src/interceptor/SetAttrLoginUserInterceptor.java
+++ src/interceptor/SetAttrLoginUserInterceptor.java
@@ -1,6 +1,7 @@
 package interceptor;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import models.Office;
 import models.UserLogin;
@@ -14,6 +15,8 @@ import com.jfinal.aop.Interceptor;
 import com.jfinal.aop.Invocation;
 import com.jfinal.core.Controller;
 import com.jfinal.log.Logger;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
 
 public class SetAttrLoginUserInterceptor implements Interceptor{
 	private Logger logger = Logger.getLogger(SetAttrLoginUserInterceptor.class);
@@ -21,19 +24,30 @@ public class SetAttrLoginUserInterceptor implements Interceptor{
 	public void intercept(Invocation ai) {
 		Subject currentUser = SecurityUtils.getSubject();
 		if(currentUser.isAuthenticated()){
-			UserLogin user = UserLogin.dao.findFirst("select * from user_login where user_name=?",currentUser.getPrincipal());
+		    UserLogin user = ai.getController().getSessionAttr("user");
+		    if(user==null){
+                user = UserLogin.dao.findFirst("select * from user_login where user_name=?",currentUser.getPrincipal());
+                ai.getController().setSessionAttr("user", user);
+		    }else{
+		        logger.debug("SetAttrLoginUserInterceptor get user from session, c_name:"+user.get("c_name") );
+		    }
+			
 			if(user.get("c_name") != null && !"".equals(user.get("c_name"))){
 				ai.getController().setAttr("userId", user.get("c_name"));
 			}else{
 				ai.getController().setAttr("userId", currentUser.getPrincipal());
 			}
 			
-			UserOffice uo = UserOffice.dao.findFirst("select * from user_office where user_name ='"+currentUser.getPrincipal()+"' and is_main=1");
-	        if(uo != null){
-	            Office office = Office.dao.findById(uo.get("office_id"));
-	            ai.getController().setAttr("office_name", office.get("office_name"));
-	        }
-	        
+			String office_name = ai.getController().getSessionAttr("office_name");
+			if(office_name==null){
+                Record uo = Db.findFirst("select o.office_name from user_office uo left join office o on uo.office_id=o.id" 
+                        +" where uo.user_name ='"+currentUser.getPrincipal()+"' and uo.is_main=1");
+                if(uo != null){
+                    ai.getController().setSessionAttr("office_name", uo.get("office_name"));
+                }
+			}else{
+                logger.debug("SetAttrLoginUserInterceptor get office_name from session:" + office_name);
+            }
 			ai.getController().setAttr("user_login_id", currentUser.getPrincipal());
 			ai.getController().setAttr("permissionMap", ai.getController().getSessionAttr("permissionMap"));
 		}
@@ -42,18 +56,24 @@ public class SetAttrLoginUserInterceptor implements Interceptor{
 	}
 	
 	private void setSysTitle(Controller controller) {
-		HttpServletRequest request = controller.getRequest();
-		String serverName = request.getServerName();
-        String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+"/";
-        
-        logger.debug("Current host path:"+basePath);
-        OfficeCofig of = OfficeCofig.dao.findFirst("select * from office_config where domain like '"+serverName +"%' or domain like '%"+serverName +"%'");
-        if(of==null){//没有配置公司的信息会导致页面出错，显示空白页
-        	of = new OfficeCofig();
-        	of.set("system_title", "易达物流");
-        	of.set("logo", "/yh/img/eeda_logo.ico");
-        }
-        controller.setAttr("SYS_CONFIG", of);
+	    OfficeCofig of = controller.getSessionAttr("SYS_CONFIG");
+	    if(of==null){
+            HttpServletRequest request = controller.getRequest();
+            String serverName = request.getServerName();
+            String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+"/";
+            
+            logger.debug("Current host path:"+basePath);
+            of = OfficeCofig.dao.findFirst("select * from office_config where domain like '"+serverName +"%' or domain like '%"+serverName +"%'");
+            if(of==null){//没有配置公司的信息会导致页面出错，显示空白页
+                of = new OfficeCofig();
+                of.set("system_title", "易达物流");
+                of.set("logo", "/yh/img/eeda_logo.ico");
+            }
+            controller.setSessionAttr("SYS_CONFIG", of);
+	    }else{
+	        logger.debug("SetAttrLoginUserInterceptor get system_title from session:" + of.getStr("system_title"));
+	    }
+	    controller.setAttr("SYS_CONFIG", of);
 	}
 
 }
