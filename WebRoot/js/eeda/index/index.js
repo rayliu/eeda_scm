c7dbcd492ab402ff234b8da5e4ddf77e61d9bf09
Ray Liu <liuyubie@gmail.com>
2016-08-30 15:05:28 +0800

    index: 添加两个图表

---
+++ WebRoot/js/eeda/index/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    document.title = '管理看板 | ' + document.title;
+
+
+
+});
---
+++ WebRoot/js/eeda/index/index_weekly_charts.js
@@ -0,0 +1,73 @@
+$(document).ready(function ()  {
+
+       // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('line_chart'));
+
+        // 指定图表的配置项和数据
+        var option = {
+            title: {
+                text: '工作周报'
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            legend: {
+                data:['电商订单','订单未支付','订单未上报','运单未上报','清单审批被拒']
+            },
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+            },
+            // toolbox: {
+            //     feature: {
+            //         saveAsImage: {}
+            //     }
+            // },
+            xAxis: {
+                type: 'category',
+                boundaryGap: false,
+                data: ['周一','周二','周三','周四','周五','周六','周日']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    name:'电商订单',
+                    type:'line',
+                    stack: '总量',
+                    data:[120, 132, 101, 134, 90, 230, 210]
+                },
+                {
+                    name:'订单未支付',
+                    type:'line',
+                    stack: '总量',
+                    data:[220, 182, 191, 234, 290, 330, 310]
+                },
+                {
+                    name:'订单未上报',
+                    type:'line',
+                    stack: '总量',
+                    data:[150, 232, 201, 154, 190, 330, 410]
+                },
+                {
+                    name:'运单未上报',
+                    type:'line',
+                    stack: '总量',
+                    data:[320, 332, 301, 334, 390, 330, 320]
+                },
+                {
+                    name:'清单审批被拒',
+                    type:'line',
+                    stack: '总量',
+                    data:[820, 932, 901, 934, 1290, 1330, 1320]
+                }
+            ]
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+
+});
