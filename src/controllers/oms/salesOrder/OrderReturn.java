1bdb72b3b200e69e1ecd0c263b7514f7488effa6
Ray Liu <liuyubie@gmail.com>
2016-12-20 15:01:53 +0800

    salesOrder 新接口修改

--- src/controllers/oms/salesOrder/OrderReturn.java
+++ src/controllers/oms/salesOrder/OrderReturn.java
@@ -1,25 +1,24 @@
 
 package controllers.oms.salesOrder;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-import models.eeda.oms.SalesOrder;
-
-import com.allinpay.ets.client.SecurityUtil;
 import com.jfinal.core.Controller;
 import com.jfinal.log.Logger;
 import com.jfinal.plugin.activerecord.Db;
-
-import config.EedaConfig;
-import controllers.oms.custom.dto.SendReturnDto;
 
 public class OrderReturn extends Controller {
     
 	private Logger logger = Logger.getLogger(OrderReturn.class);
     
 	public void orderResultRecv(){
-		String resultMsg = getPara("cebJsonMsg");
+		String resultMsg = getPara("cebJsonMsg");//www-form-urlencoded
 		System.out.println(resultMsg);
 	    Db.update("update customize_field set field_code = ? where order_type = 'returnMassge'",resultMsg);
-		renderJson();
+		
+//	    Map map=new HashMap();
+//	    map.put("status", "success");
+//	    renderJson(map);
+	    renderText("success");
 	}
 }
