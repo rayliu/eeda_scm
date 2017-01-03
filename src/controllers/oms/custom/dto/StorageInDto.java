cb08dc5ec84b09ce7b3e48476ab2799a45f2e6e8
Kevin <kevin.dou@eeda123.com>
2017-01-03 18:14:20 +0800

    直购入库单new java文件

---
+++ src/controllers/oms/custom/dto/StorageInDto.java
@@ -0,0 +1,104 @@
+package controllers.oms.custom.dto;
+
+public class StorageInDto {
+    private String org_code;
+    private String customs_code;
+    private String cop_no;
+    private String operator_code;
+    private String operator_name;
+    private String traf_mode;
+    private String traf_no;
+    private String voyage_no;
+    private String bill_no;
+    private String logistics_code;
+    private String logistics_name;
+    private String unload_location;
+    private String logistics_nos;
+    private String note;
+    
+	public String getOrg_code() {
+		return org_code;
+	}
+	public void setOrg_code(String org_code) {
+		this.org_code = org_code;
+	}
+	public String getCustoms_code() {
+		return customs_code;
+	}
+	public void setCustoms_code(String customs_code) {
+		this.customs_code = customs_code;
+	}
+	public String getCop_no() {
+		return cop_no;
+	}
+	public void setCop_no(String cop_no) {
+		this.cop_no = cop_no;
+	}
+	public String getOperator_code() {
+		return operator_code;
+	}
+	public void setOperator_code(String operator_code) {
+		this.operator_code = operator_code;
+	}
+	public String getOperator_name() {
+		return operator_name;
+	}
+	public void setOperator_name(String operator_name) {
+		this.operator_name = operator_name;
+	}
+	public String getTraf_mode() {
+		return traf_mode;
+	}
+	public void setTraf_mode(String traf_mode) {
+		this.traf_mode = traf_mode;
+	}
+	public String getTraf_no() {
+		return traf_no;
+	}
+	public void setTraf_no(String traf_no) {
+		this.traf_no = traf_no;
+	}
+	public String getVoyage_no() {
+		return voyage_no;
+	}
+	public void setVoyage_no(String voyage_no) {
+		this.voyage_no = voyage_no;
+	}
+	public String getBill_no() {
+		return bill_no;
+	}
+	public void setBill_no(String bill_no) {
+		this.bill_no = bill_no;
+	}
+	public String getLogistics_code() {
+		return logistics_code;
+	}
+	public void setLogistics_code(String logistics_code) {
+		this.logistics_code = logistics_code;
+	}
+	public String getLogistics_name() {
+		return logistics_name;
+	}
+	public void setLogistics_name(String logistics_name) {
+		this.logistics_name = logistics_name;
+	}
+	public String getUnload_location() {
+		return unload_location;
+	}
+	public void setUnload_location(String unload_location) {
+		this.unload_location = unload_location;
+	}
+	public String getLogistics_nos() {
+		return logistics_nos;
+	}
+	public void setLogistics_nos(String logistics_nos) {
+		this.logistics_nos = logistics_nos;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+}
---
+++ src/models/StorageInOrder.java
@@ -0,0 +1,1 @@
+package models;import com.jfinal.plugin.activerecord.Model;@SuppressWarnings("serial")public class StorageInOrder extends Model<StorageInOrder> {	public static final StorageInOrder dao = new StorageInOrder();}
