5c017ddad410020e0f5fdbd267dfe3f98d24fe92
Kevin <kevin.dou@eeda123.com>
2017-01-05 10:04:14 +0800

    文件提交回滚

--- WebRoot/eeda/js/eeda/eeda.js
+++
@@ -1,9 +0,0 @@
-var eeda={}||eeda;
-
-eeda.getQueryStringRegExp =function(name)
-    {
-        var reg = new RegExp("(^|\\?|&)"+ name +"=([^&]*)(\\s|&|$)", "i");  
-        if (reg.test(location.href)) 
-            return decodeURI(RegExp.$2.replace(/\+/g, " ")); 
-        return "";
-    };
